75f902a126f53a449808e75d55ae8dd85ccb47d1
Ryanwuu <Ryanwuu@outlook.com>
2015-01-12 01:22:46 +0800

    Implement Instance Methods

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 module.exports = Class;
-function Class(func){
-	if (func['initialize']){
-		return func['initialize'];
-	}
-	return function(){
-		
-	}
 
+function Class(argument){
+	var cons = function(){};
+	if (argument['initialize']){
+		cons = argument['initialize'];
+	}
+	var name ='';
+	for(var name in argument){
+		if (name !== 'initialize'){
+			cons.prototype[name] = argument[name];
+		}
+	}
+	return cons;
 }
